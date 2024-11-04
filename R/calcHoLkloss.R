4b9602689ea9ec620700052c89279c2ff28d3b54
YSL Proteomics <yslproteomics@gmail.com>
2024-11-04 13:41:05 -0500

    Update calHoLkloss documentation

--- R/calcHoLkloss.R
+++ R/calcHoLkloss.R
@@ -1,43 +1,58 @@
-#' Calculate ln(H/L+1)-based k_loss for all peptides
+#' Calculate Logarithmic H/L-based k_loss for all Peptides
 #'
-#' A wrapper to apply getHoLmod to all peptides of the dataset.
+#' This function calculates the ln(H/L + 1)-based k_loss for each peptide in a `pSILAC` dataset by applying the `getHoLmod` function. 
+#' It handles data across multiple time points and samples, with optional parallel processing to speed up computations.
 #'
-#' @param e a pSILAC object.
+#' @param o A `pSILAC` object, which contains the data for peptide H/L ratios and associated metadata.
+#' @param ncores An integer specifying the number of cores for parallel processing. If `NULL`, it defaults to using all available cores minus one.
 #'
-#' @return The updated pSILAC object.
+#' @return The updated `pSILAC` object, with a new `hol.kloss` slot containing the calculated k_loss values and associated statistics (standard error, sum of squared residuals, and number of points) for each peptide.
+#'
+#' @details
+#' The function iterates over each unique sample in the dataset and calculates k_loss values for each peptide, either in parallel or sequentially. 
+#' For each sample, the calculated metrics include the k_loss, its standard error, sum of squared residuals, and the number of points used in the calculation.
+#' A summary message is displayed for each sample, indicating the number of successfully calculated k_loss values and any missing values.
 #'
 #' @export
-calcHoLkloss <- function(e, ncores=NULL){
-  if(class(e) != "pSILAC")	stop("e should be a pSILAC object.")
-  e$hol.kloss <- NULL
-  x <- unique(e$design$time)
-  if(is.null(ncores)){
+calcHoLkloss <- function(o, ncores = NULL) {
+  if (class(o) != "pSILAC") stop("o should be a pSILAC object.")
+  
+  o$hol.kloss <- NULL
+  x <- unique(o$design$time)
+  
+  if (is.null(ncores)) {
     library(parallel)
     ncores <- detectCores() - 1
-  }else{
-    if(ncores>1)	library(parallel)
-  }
-  if(ncores>1){
+  } else if (ncores > 1) {
     library(parallel)
+  }
+  
+  if (ncores > 1) {
     cl <- makeCluster(ncores)
     clusterExport(cl, c("getHoLmod"))
-    clusterExport(cl, c("x","e"), environment())
+    clusterExport(cl, c("x", "o"), environment())
   }
-  for(p in unique(e$design$sample)){
-    if(ncores > 1){
-      d <- as.data.frame(t(parApply(cl,e$hol[,which(e$design$sample==p)],1,FUN=function(y){ getHoLmod(x,y) })))
-    }else{
-      d <- as.data.frame(t(apply(e$hol[,which(e$design$sample==p)],1,FUN=function(y){ getHoLmod(x,y) })))
+  
+  for (p in unique(o$design$sample)) {
+    if (ncores > 1) {
+      d <- as.data.frame(t(parApply(cl, o$hol[, which(o$design$sample == p)], 1, FUN = function(y) { getHoLmod(x, y) })))
+    } else {
+      d <- as.data.frame(t(apply(o$hol[, which(o$design$sample == p)], 1, FUN = function(y) { getHoLmod(x, y) })))
     }
-    colnames(d) <- paste(p,c("kloss","kloss.stderr","kloss.SSR","nbpoints"),sep=".")
-    row.names(d) <- row.names(e$hol)
-    message(paste(" ...calculated ",sum(!is.na(d[,1]))," k_loss values for sample ",p," (",sum(is.na(d[,1]))," missing)",sep=""))
-    if(is.null(e$hol.kloss)){
-      e$hol.kloss <- d
-    }else{
-      e$hol.kloss <- cbind(e$hol.kloss,d)
+    
+    colnames(d) <- paste(p, c("kloss", "kloss.stderr", "kloss.SSR", "nbpoints"), sep = ".")
+    row.names(d) <- row.names(o$hol)
+    
+    message(sprintf("...calculated %d k_loss values for sample %s (%d missing)", sum(!is.na(d[, 1])), p, sum(is.na(d[, 1]))))
+    
+    if (is.null(o$hol.kloss)) {
+      o$hol.kloss <- d
+    } else {
+      o$hol.kloss <- cbind(o$hol.kloss, d)
     }
   }
-  if(ncores > 1) stopCluster(cl)
-  return(e)
+  
+  if (ncores > 1) stopCluster(cl)
+  
+  return(o)
 }
