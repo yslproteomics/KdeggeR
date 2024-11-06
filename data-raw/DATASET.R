15ae7fb0bb11db4d8edc85929ac212eaecd87b98
YSL Proteomics <yslproteomics@gmail.com>
2024-11-06 15:39:15 -0500

    Provide raw data

---
+++ data-raw/DATASET.R
@@ -0,0 +1,7 @@
+## code to prepare `DATASET` dataset goes here
+
+usethis::use_data(example_spectronaut, overwrite = TRUE)
+
+usethis::use_data(example_spectronaut_design, overwrite = TRUE)
+
+usethis::use_data(example_spectronaut_design_replicates, overwrite = TRUE)
