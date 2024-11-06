7e6aafb80701489a32ce4c27aeb237daa76df1e2
YSL Proteomics <yslproteomics@gmail.com>
2024-11-06 15:44:25 -0500

    Update calcAllRates.R
    update documentation

--- R/calcAllRates.R
+++ R/calcAllRates.R
@@ -5,16 +5,16 @@
 #'
 #' @param o A `pSILAC` object containing the data on which k_loss calculations will be performed.
 #' @param method A character string specifying the method used for calculating protein-level k_loss rates. 
-#'   Default is `"combined"`. For more options and details, see the documentation for `calcProteinsKloss`.
+#'   Default is `combined`. For more options and details, see the documentation for `calcProteinsKloss`.
 #' @param ag.metric A character string specifying the aggregation metric used for calculating protein-level rates.
-#'   Default is `"mean"`. Additional options can be found in `calcProteinsKloss`.
-#' @param ag.weights A character string defining the method for calculating weights in aggregation (default is `"variance"`). 
-#'   This parameter is ignored if `ag.metric` is not set to `"mean"`. Refer to `calcProteinsKloss` for details.
+#'   Default is `mean`. Additional options can be found in `calcProteinsKloss`.
+#' @param ag.weights A character string defining the method for calculating weights in aggregation (default is `variance`). 
+#'   This parameter is ignored if `ag.metric` is not set to `mean`. Refer to `calcProteinsKloss` for details.
 #' @param in.all Logical or numeric indicating whether to use only peptides quantified in all samples.
 #'   For more information, refer to `calcProteinsKloss`.
 #' @param ncores An integer specifying the number of cores to use for parallel processing.
 #' @param tryRobust Logical, indicating if a robust fitting method should be used when applicable. Default is `FALSE`.
-#' @param startIntensity Character string, specifying the initial intensity value used in calculations. Default is `"max"`.
+#' @param startIntensity Character string, specifying the initial intensity value used in calculations. Default is `max`.
 #'
 #' @return The function returns the updated `pSILAC` object with added k_loss estimates for both peptides and proteins.
 #'   The updated object includes protein-level k_loss rates calculated according to the specified method and aggregation metric.
