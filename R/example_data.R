c5d1ffd297b85eb56c02bdaead4e50778c64a1c6
YSL Proteomics <yslproteomics@gmail.com>
2024-11-07 14:21:50 -0500

    Update example data
    Add DIA-NN data and kcd example files.

--- R/example_data.R
+++ R/example_data.R
@@ -19,3 +19,18 @@
 #'Note, the replicate number must be specified and the grouping must be reflected in the sample column. 
 #'
 "example_spectronaut_design_replicates"
+
+#'@title Example pSILAC dataset analyzed in DIA-NN 1.9
+#'
+#'@description This dataset contains a data.frame containing the first 20,000 unique precursors
+#'from the A2780Cis and A2780 parental cell line dataset. This dataset contains 3 dish (biological) replicates
+#'and 4 labeling time points of 1, 4, 12, and 24 hours. 
+#'The data were analyzed and exported from DIA-NN 1.9. 
+#'
+"example_diann"
+
+#'@title Example Kcd file
+#'
+#'@description This file contains Kcd values of the A2780 and A2780Cis cell lines. 
+#'
+"example_kcd"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,3 +5,7 @@ usethis::use_data(example_spectronaut, overwrite = TRUE)
 usethis::use_data(example_spectronaut_design, overwrite = TRUE)
 
 usethis::use_data(example_spectronaut_design_replicates, overwrite = TRUE)
+
+usethis::use_data(example_diann, overwrite = TRUE)
+
+usethis::use_data(example_kcd, overwrite = TRUE)
